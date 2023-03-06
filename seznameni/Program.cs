060ecdf0665e1662b91d14506006f28e3df7c202
tomas.pabian <tomas.pabian@VT1-05>
2023-03-06 10:41:52 +0100

    Přidejte soubory projektu.

---
+++ seznameni/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] zajmy = {"Zdravím, jmenuji se Tomáš 'Kvéčko' Pabián, aktuálně studuji na SŠ Educhem",
+                    "", "Mezi mé oblíbené věci patří:", "Hry a obzvlášť FPSka", "Ping Pong",
+                    "average vesnice > město chad"};
+
+Console.WriteLine("");
+
+foreach (var item in zajmy)
+{
+    Console.WriteLine(item);
+}
